ba52f366651921eb277fdcd303bff81f2c5a0cc7
mfl <re-imagine.studio@proton.me>
2025-11-21 16:44:35 +0100

    Apuntes Java I

---
+++ src/_1_Estructuras_Secuenciales_I.java
@@ -0,0 +1,8 @@
+public class _1_Estructuras_Secuenciales_I {
+    public static void main(String[] args) {
+        /* Realiza un programa que muestre por consola el texto "Hola Mundo" */
+
+        String saludo = "Hola Mundo!";
+        System.out.println(saludo);
+    }
+}
